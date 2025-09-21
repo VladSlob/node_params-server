4869ae23d0ad379ab9f2ec6e4d1e76ce604dc073
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-09-21 16:32:30 +0300

    createServer.js

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,32 @@
 /* eslint-disable no-console */
 'use strict';
 
+const http = require('http');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    const params = new URL(req.url, 'http://localhost');
+    const parts = params.pathname.split('/').filter(Boolean);
+    const query = {};
+
+    console.log(params.searchParams);
+
+    for (const [key, value] of params.searchParams) {
+      query[key] = String(value);
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.write(
+      JSON.stringify({
+        parts,
+        query,
+      }),
+    );
+    res.end();
+  });
+
+  return server;
 }
 
 module.exports = {
