7cda2b07aa428cb42d60d7e14262afa3baa84290
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-09-21 16:32:10 +0300

    app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@
 'use strict';
 
 const { createServer } = require('./createServer');
+const axios = require('axios');
+const PORT = process.env.PORT || 5700;
 
-createServer().listen(5701, () => {
-  console.log(`Server is running on http://localhost:${5701} 🚀`);
-  console.log('Available at http://localhost:5700');
-});
+const server = createServer();
+
+server.listen(PORT);
+
+axios
+  .get(`http://localhost:${PORT}/hello/world/123?x=1&search=some`)
+  .then((response) => {
+    console.log('GET response:', response.data);
+  })
+  .catch((error) => {
+    console.error('Error during GET request:', error.message);
+  });
